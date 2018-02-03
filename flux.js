d81dca7bb94b9ddbb0e985f574690042bb20fd09
aleclarson <alec.stanford.larson@gmail.com>
2018-02-03 10:22:13 -0500

    add `name` property to each pre-made queue

--- flux.js
+++ flux.js
@@ -2,8 +2,10 @@ const main = require('.')
 
 // High priority
 const high = main.queue(2000)
+high.name = 'flux.high'
 exports.high = high.push
 
 // Low priority
 const low = main.queue(200)
+low.name = 'flux.low'
 exports.low = low.push
--- index.js
+++ index.js
@@ -6,8 +6,10 @@ module.exports = main
 
 // High priority
 const high = main.queue(1000)
+high.name = 'weaver.high'
 main.high = high.push
 
 // Low priority
 const low = main.queue(100)
+low.name = 'weaver.low'
 main.low = low.push
